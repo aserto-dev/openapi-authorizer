8913eb3b04063e05476a8743e8ff8e44516c55d0
Gimmy <gheorghe.popescu@aserto.com>
2025-02-14 11:10:40 +0200

    Add OpenAPI handler with templated server

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -11,7 +11,6 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
-	"text/template"
 	"unicode"
 	"unicode/utf8"
 
@@ -52,7 +51,6 @@ func Deps() {
 
 // Generate the code
 func Generate() error {
-
 	tag, err := buf.GetLatestTag(bufImage)
 	if err != nil {
 		fmt.Println("Could not retrieve tags, using latest")
@@ -93,11 +91,6 @@ func gen(bufImage, fileSources, version string) error {
 	}
 
 	err = publishOpenAPIv3(version)
-	if err != nil {
-		return err
-	}
-
-	err = generateEmbedCode()
 	if err != nil {
 		return err
 	}
@@ -323,29 +316,6 @@ func publishOpenAPIv3(version string) error {
 	return nil
 }
 
-func generateEmbedCode() error {
-	t, err := template.New("embed").Parse(embedTemplate)
-	if err != nil {
-		return err
-	}
-
-	targets := []string{
-		"publish/authorizer/openapi.go",
-	}
-	for _, target := range targets {
-		w, err := os.Create(target)
-		if err != nil {
-			return err
-		}
-
-		err = t.Execute(w, nil)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 func fileExists(filepath string) bool {
 	info, err := os.Stat(filepath)
 	if os.IsNotExist(err) {
@@ -379,16 +349,3 @@ func Clean() error {
 	}
 	return os.RemoveAll("publish")
 }
-
-const embedTemplate string = `package openapi
-
-import "embed"
-
-//go:embed openapi.json
-var staticAssets embed.FS
-
-// Static embedded FS service openapi.json file.
-func Static() embed.FS {
-	return staticAssets
-}
-`
